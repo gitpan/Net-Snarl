20cdf55a84c9ad9c99a8c72b6f1ed08f8583fe20
Alan Berndt <aberndt@cpan.org>
2013-04-02 16:17:30 +0000

    Import of ABERNDT/Net-Snarl-1.08.tar.gz from CPAN.
    gitpan-cpan-distribution: Net-Snarl
    gitpan-cpan-version:      1.08
    gitpan-cpan-path:         ABERNDT/Net-Snarl-1.08.tar.gz
    gitpan-cpan-author:       ABERNDT
    gitpan-cpan-maturity:     released

--- lib/Net/Snarl.pm
+++ lib/Net/Snarl.pm
@@ -1,7 +1,7 @@
 ## no critic
 package Net::Snarl;
 {
-  $Net::Snarl::VERSION = '1.07';
+  $Net::Snarl::VERSION = '1.08';
 }
 ## use critic
 
@@ -17,7 +17,7 @@ Net::Snarl - Snarl network protocol
 
 =head1 VERSION
 
-version 1.07
+version 1.08
 
 =cut
 
